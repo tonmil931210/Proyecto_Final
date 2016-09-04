e96692432ca7cac2c9fb1ce2f1608bf42acb7a05
Andres Vera <andoresu95@gmail.com>
2016-09-04 18:32:31 -0500

    login

--- app/scripts/services/loginService.js
+++ app/scripts/services/loginService.js
@@ -26,7 +26,8 @@ angular.module('bodegaUninorteApp')
 				data: {email: userData.email, password: userData.password}
 			}).then(function successCallback(response) {
 			    //STOP LOANDING ANIMATION
-			    console.log(response);			    
+			    console.log(response);
+			    console.log(response.headers());			    
 			    console.log(response.headers(['Authorization']));			    
 			    console.log(response.headers(['Date']));			    
 			    if(userData.remember_me){
