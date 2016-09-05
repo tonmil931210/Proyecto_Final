12525cad0a8b30c630cc2a3cae18294418f281c4
Andres Vera <andoresu95@gmail.com>
2016-09-05 03:09:22 -0500

    login

--- app/scripts/services/itemsService.js
+++ app/scripts/services/itemsService.js
@@ -6,12 +6,12 @@ angular.module('bodegaUninorteApp')
 		all: function (){
 			$http({
 				method: 'GET',
-				url: urlConstant + ''
+				url: urlConstant + 'items'
 			}).then(function successCallback(response) {
 			    //STOP LOANDING ANIMATION
-			    
+			    return response.data.items;
 			  }, function errorCallback(response) {
-
+			  	return null;
 			  });			
 		},
 		new: function (itemData) {
@@ -22,13 +22,12 @@ angular.module('bodegaUninorteApp')
 					Authorization: sessionService.get('token')
 				},
 				data: {
-					name: itemData,
+					name: itemData.name,
 					item_type_id: 0,
 					number: itemData.quantity,
 					min_stock: itemData.minStock,
 					price: itemData.price,
 					recorder: itemData.reorder
-
 				}
 			}).then(function successCallback(response) {
 			    //STOP LOANDING ANIMATION
@@ -36,6 +35,89 @@ angular.module('bodegaUninorteApp')
 				}, function errorCallback(response) {
 					console.log('todo salio mal');
 			  	});
+		},
+		get: function (itemId) {
+			$http({
+			 	method: 'GET',
+			 	url: urlConstant + 'items/' + itemId,
+			 	headers:{
+			 		Authorization: sessionService.get('token')
+			 	}
+			 }).then(function successCallback(response) {
+			 	return response.data.item;
+			 }, function errorCallBack(response) {
+		 	 	return null;
+			 });
+		},
+		edit: function (itemData) {
+			$http({
+			 	method: 'PUT',
+			 	url: urlConstant + 'items/' + itemData.id,
+			 	headers:{
+			 		Authorization: sessionService.get('token')
+			 	},
+			 	data: {
+					name: itemData.name,
+					item_type_id: 0,
+					number: itemData.quantity,
+					min_stock: itemData.minStock,
+					price: itemData.price,
+					recorder: itemData.reorder
+				}
+			 }).then(function successCallback(response) {
+			 	console.log("item con id " + itemData.id + " editado exitosamente");
+			 }, function errorCallBack(response) {
+		 	 	console.log("item con id " + itemData.id + " NO FUE editado exitosamente");
+			 });	 
+		},
+		delete: function (itemId) {
+			 $http({
+			 	method: 'DELETE',
+			 	url: urlConstant + 'items/' + itemId,
+			 	headers:{
+			 		Authorization: sessionService.get('token')
+			 	}
+			 }).then(function successCallback(response) {
+			 	console.log("item con id " + itemId + " borrado exitosamente");
+			 }, function errorCallBack(response) {
+		 	 	console.log("item con id " + itemId + " NO FUE borrado exitosamente");
+			 });
+		},
+		getItemsForReorder: function () {
+			$http({
+				method: 'GET',
+				url: urlConstant + 'items'
+			}).then(function successCallback(response) {
+			    //STOP LOANDING ANIMATION
+			    var items = response.data.items;
+			    var itemsReorder = [];
+			    for(var item of items){
+			    	if(item.quantity < item.reorder && item.quantity >= item.minStock){
+		      			itemsReorder.push(item);
+		      		}
+			    }
+			    return itemsReorder;
+			  }, function errorCallback(response) {
+			  	return null;
+			  });
+		},
+		getItemsMinStock: function () {
+			 $http({
+				method: 'GET',
+				url: urlConstant + 'items'
+			}).then(function successCallback(response) {
+			    //STOP LOANDING ANIMATION
+			    var items = response.data.items;
+			    var itemsMinStock = [];
+			    for(var item of items){
+			    	if(item.quantity < item.minStock){
+		      			itemsMinStock.push(item);
+		      		}
+			    }
+			    return itemsMinStock;
+			  }, function errorCallback(response) {
+			  	return null;
+			  });
 		}
 	};
 });
